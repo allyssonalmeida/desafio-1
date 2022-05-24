d2392d87ed00184df91698aabbf3f5da77fb07d3
Allysson Fernando Guedes de Almeida <allysson.guedes@trinto.com.br>
2022-05-24 20:07:58 -0300

    first commit

---
+++ 9.js
@@ -0,0 +1,12 @@
+let d = new Date();
+console.log(d);
+var l = new ArrayList();
+console.log(l)
+var o = new Object();
+console.log(o);
+var a = new Array();
+console.log(a)
+var x = new Date(); //nome alterado para evitar conflito com a variável d criada previamente
+console.log(d);
+
+//Alternativa B
